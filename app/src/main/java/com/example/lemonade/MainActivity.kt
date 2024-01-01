34aa75b92f3f63749a7dcbcf98c641b68dd59e1b
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-01 16:54:43 -0500

    added state-based logic to determine what to display in the UI and how to handle a tap

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.lemonade
 
-import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,12 +13,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -28,8 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -39,7 +34,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,7 +54,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainContent(modifier: Modifier = Modifier) {
-    var step by remember { mutableIntStateOf(0) }
+    var stepNumber by remember { mutableIntStateOf(0) }
+
+    val step = when(stepNumber) {
+        0 -> Step(
+            currentStep = stepNumber,
+            description = stringResource(id = R.string.tree_description),
+            image = painterResource(id = R.drawable.lemon_tree),
+            instructions = stringResource(id = R.string.tree_instructions),
+        )
+        1 -> QuicktimeStep(
+            currentStep = stepNumber,
+            description = stringResource(id = R.string.lemon_description),
+            image = painterResource(id = R.drawable.lemon_squeeze),
+            instructions = stringResource(id = R.string.lemon_instructions),
+        )
+        2 -> Step(
+            currentStep = stepNumber,
+            description = stringResource(id = R.string.lemonade_description),
+            image = painterResource(id = R.drawable.lemon_drink),
+            instructions = stringResource(id = R.string.lemonade_instructions),
+        )
+        else -> Step(
+            currentStep = stepNumber,
+            description = stringResource(id = R.string.empty_glass_description),
+            image = painterResource(id = R.drawable.lemon_restart),
+            instructions = stringResource(id = R.string.empty_glass_instructions),
+        )
+    }
 
     Column {
          Row(modifier = modifier
@@ -78,34 +99,42 @@ fun MainContent(modifier: Modifier = Modifier) {
          }
 
          Row(modifier = modifier.weight(weight = 1.0f)) {
-            Instructions(modifier = modifier, onClick = { step = (step + 1) % 4}, step = step)
+            Instructions(modifier = modifier, onChange = { stepNumber = it }, step = step)
          }
     }
 }
 
+open class Step(
+    protected val currentStep: Int,
+    val description: String,
+    val instructions: String,
+    val image: Painter,
+) {
+    open fun onClick(): Int {
+        return (currentStep + 1) % 4
+    }
+}
+
+class QuicktimeStep(
+    currentStep: Int,
+    description: String,
+    instructions: String,
+    image: Painter,
+): Step(currentStep, description, instructions, image) {
+    private var numTaps = (2..4).random()
+    private var tapCount = 0
+
+    override fun onClick(): Int {
+        if(++tapCount < numTaps) {
+            return currentStep
+        }
+
+        return super.onClick()
+    }
+}
+
 @Composable
-fun Instructions(modifier: Modifier = Modifier, onClick: () -> Unit, step: Int) {
-    val description = when(step) {
-        0 -> stringResource(id = R.string.tree_description)
-        1 -> stringResource(id = R.string.lemon_description)
-        2 -> stringResource(id = R.string.lemonade_description)
-        else -> stringResource(id = R.string.empty_glass_description)
-    }
-
-    val image = when(step) {
-        0 -> painterResource(id = R.drawable.lemon_tree)
-        1 -> painterResource(id = R.drawable.lemon_squeeze)
-        2 -> painterResource(id = R.drawable.lemon_drink)
-        else -> painterResource(id = R.drawable.lemon_restart)
-    }
-
-    val instructions = when (step) {
-        0 -> stringResource(id = R.string.tree_instructions)
-        1 -> stringResource(id = R.string.lemon_instructions)
-        2 -> stringResource(id = R.string.lemonade_instructions)
-        else -> stringResource(id = R.string.empty_glass_instructions)
-    }
-    
+fun Instructions(modifier: Modifier = Modifier, onChange: (Int) -> Unit, step: Step) {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -114,18 +143,18 @@ fun Instructions(modifier: Modifier = Modifier, onClick: () -> Unit, step: Int) 
         Button(
             colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.button_background)),
             modifier = modifier,
-            onClick = onClick,
+            onClick = { onChange(step.onClick()) },
             shape = RoundedCornerShape(percent = 15)
         ) {
             Image(
-                contentDescription = description,
-                painter = image,
+                contentDescription = step.description,
+                painter = step.image,
             )
         }
 
         Text(
             modifier = modifier.padding(16.dp),
-            text = instructions
+            text = step.instructions
         )
     }
 }
